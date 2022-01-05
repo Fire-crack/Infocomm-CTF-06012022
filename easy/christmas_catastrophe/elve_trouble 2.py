6c75597ecd82008015b0d22aea83eb7add3c5902
Fire-crack <84394443+Fire-crack@users.noreply.github.com>
2022-01-05 15:09:48 +0800

    Elves are real too

---
+++ easy/christmas_catastrophe/elve_trouble 2.py
@@ -0,0 +1,13 @@
+# Okay I know it's not Christmas at the time of you solving it, but these puzzles were made before the date of the CTF was fixed
+# So cut me some slack okay
+# - Izo
+# 
+# This question is a continuation of elve_trouble, please read that first to solve this challenge.
+# 
+# Now, the elves need you to help them again!
+# 
+# Using the same criteria, they need you to find three different numbers that sum to 2021
+# 
+# In your expense report, what is the product of the three entries that sum to 2021?
+# 
+# (The flag will be CTFInfocomm{product})
