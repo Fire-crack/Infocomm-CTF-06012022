e28f864cb72239392db33b2e7a414bd11f680f49
Fire-crack <84394443+Fire-crack@users.noreply.github.com>
2022-01-05 15:09:09 +0800

    Santa is real

---
+++ easy/christmas_catastrophe/elve_trouble.py
@@ -0,0 +1,26 @@
+# Okay I know it's not Christmas at the time of you solving it, but these puzzles were made before the date of the CTF was fixed
+# So cut me some slack okay
+# - Izo
+# 
+# (There's a continuation of this question at "elve trouble 2", at easy difficulty)
+# 
+# Christmas disaster has struck!
+# The Elves helping Santa out has a little trouble with their accounting
+# 
+# They just need you to fix your expense report, apparently, something isn't quite adding up.
+# 
+# Specifically, they need you to find the two entries that sum to 2021 and then multiply those two numbers together.
+# 
+# For example, suppose your expense report contained the following:
+# 
+# 1722
+# 979
+# 366
+# 299
+# 675
+# 1456
+# In this list, the two entries that sum to 2021 are 1722 and 299. Multiplying them together produces 1722 * 299 = 514878, so the correct answer is 514878.
+# 
+# (And the flag is CTFInfocomm{514878})
+# 
+# Of course, your expense report is much larger. Find the two entries that sum to 2021; what do you get if you multiply them together?
