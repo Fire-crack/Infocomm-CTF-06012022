15ac6a258feed6f509dccec518b4d66a75825153
Fire-crack <84394443+Fire-crack@users.noreply.github.com>
2022-01-05 15:03:20 +0800

    I hope your sanity is still there...

---
+++ easy/sanity_check.py
@@ -0,0 +1,5 @@
+# flag = something
+
+cipherlist = list(flag)
+
+# cipherlist = ['C', 'T', 'F', 'I', 'n', 'f', 'o', 'c', 'o', 'm', 'm', '{', 'n', 'o', '_', 'l', 'e', 'e', 'k', '_', 'f', 'l', 'a', 'g', '_', 'p', 'l', 's', '}']
